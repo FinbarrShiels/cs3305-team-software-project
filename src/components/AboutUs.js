af0fc4f7b9b5a4dd65bf3589ae13e84fa6990c83
Luke Tynan <luketynan2000@gmail.com>
2021-02-07 17:59:23 +0000

    AboutUs.js should have been in previous commit

---
+++ src/components/AboutUs.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class AboutUs extends Component {
+    render() {
+      return(
+        <h1>About Us section in progress</h1>
+      )
+    }
+  }
+  
+  export default AboutUs;
