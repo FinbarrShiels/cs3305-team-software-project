3bfad4c48071f3c051c567f9ee7797a99c2f7907
Luke Tynan <luketynan2000@gmail.com>
2021-02-07 18:42:01 +0000

    Added basic structure to sign up page
    Including main registration section with left/right tabs
    Right tab includes sign up form inputs, agree to Ts&Cs checkbox, submit button and login button for existing members

--- src/components/signUp.js
+++ src/components/signUp.js
@@ -3,7 +3,36 @@ import React, { Component } from 'react';
 class signUp extends Component {
   render() {
     return(
-      <div><h1>Once logged in , Sign up could change to Election Screen </h1></div>
+      // <div><h1>Once logged in , Sign up could change to Election Screen </h1></div>
+
+      <div className="mainSignUp">
+        <div className="leftTab">
+          <h2>Cast Your Vote <span>Now!</span></h2>
+        </div>
+
+        <div className="rightTab">
+          <form>
+            <div className="formInputSection">
+              <label for="fname">First Name:</label>
+              <input type="text" id="fname"/>
+            </div>
+            <div className="formInputSection">
+              <label for="sname">Second Name:</label>
+              <input type="text" id="sname"/>
+            </div>
+            <div className="checkboxSection">
+              <input type="checkbox" id="agreecheckbox"/>
+              <label for="agreecheckbox">I agree to terms of Service</label>
+            </div>
+            <input className="formSubmitButton" type="submit" value="Create Account"/>
+          </form>
+
+          <div className="existingMemberLogin">
+            <span>Already Have an Account?</span>
+            <button>Log In</button>
+          </div>
+        </div>
+      </div>
     )
   }
 }
