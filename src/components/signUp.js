beaf7d6629be2440182f666ad8cc2ef8017262e6
Luke Tynan <luketynan2000@gmail.com>
2021-02-10 23:38:05 +0000

    Fixed bug where LogIn button on SignUp page did not work

--- src/components/signUp.js
+++ src/components/signUp.js
@@ -15,30 +15,30 @@ class signUp extends Component {
         <div className="rightTab">
           <form class="signUpForm">
             <div className="formInputSection">
-              <label for="fname">First Name:  </label>
+              <label for="fname"> First Name:  </label>
               <input type="text" id="fname"/>
             </div>
             <div className="formInputSection">
-              <label for="sname">Second Name:  </label>
+              <label for="sname"> Second Name:  </label>
               <input type="text" id="sname"/>
             </div>
             <div className="formInputSection">
-              <label htmlFor="email">Email:  </label>
+              <label htmlFor="email"> Email:  </label>
               <input type="text" id="email"/>
             </div>
             <div className="formInputSection">
-              <label htmlFor="password">Password:  </label>
+              <label htmlFor="password"> Password:  </label>
               <input type="password" id="password"/>
             </div>
             <div className="checkboxSection">
               <input type="checkbox" id="agreecheckbox"/>
-              <label for="agreecheckbox">I agree to terms of Service</label>
+              <label for="agreecheckbox"> I agree to terms of Service </label>
             </div>
             <input className="formSubmitButton" type="submit" value="Create Account"/>
           </form>
           <div className="existingMemberLogin">
             <span>Already Have an Account?   </span>
-            <button>Log In</button>
+            <a href="/login"><button> Log In </button></a>
           </div>
 
         </div>
