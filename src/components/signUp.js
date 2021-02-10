4c13a490c47d27f4e18855b8443492f3ca6207df
FinbarrShiels <>
2021-02-10 16:05:11 +0000

    Initial update of signUp.css and signUp.js

--- src/components/signUp.js
+++ src/components/signUp.js
@@ -1,24 +1,34 @@
 import React, { Component } from 'react';
+import './signUp.css';
 
 class signUp extends Component {
   render() {
     return(
       // <div><h1>Once logged in , Sign up could change to Election Screen </h1></div>
 
-      <div className="mainSignUp">
+      <div className="signUpContainer">
+        <div class="sections">
         <div className="leftTab">
-          <h2>Cast Your Vote <span>Now!</span></h2>
+          <h2>Cast Your <span>Vote</span> Now!</h2>
         </div>
 
         <div className="rightTab">
-          <form>
+          <form class="signUpForm">
             <div className="formInputSection">
-              <label for="fname">First Name:</label>
+              <label for="fname">First Name:  </label>
               <input type="text" id="fname"/>
             </div>
             <div className="formInputSection">
-              <label for="sname">Second Name:</label>
+              <label for="sname">Second Name:  </label>
               <input type="text" id="sname"/>
+            </div>
+            <div className="formInputSection">
+              <label htmlFor="email">Email:  </label>
+              <input type="text" id="email"/>
+            </div>
+            <div className="formInputSection">
+              <label htmlFor="password">Password:  </label>
+              <input type="password" id="password"/>
             </div>
             <div className="checkboxSection">
               <input type="checkbox" id="agreecheckbox"/>
@@ -26,11 +36,12 @@ class signUp extends Component {
             </div>
             <input className="formSubmitButton" type="submit" value="Create Account"/>
           </form>
-
           <div className="existingMemberLogin">
-            <span>Already Have an Account?</span>
+            <span>Already Have an Account?   </span>
             <button>Log In</button>
           </div>
+
+        </div>
         </div>
       </div>
     )
