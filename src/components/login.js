00e932580c294b19f016786e9d9eacdd5a8b769a
FinbarrShiels <>
2021-02-10 13:59:22 +0000

    Update login.css and login.js

--- src/components/login.js
+++ src/components/login.js
@@ -1,38 +1,45 @@
 import React, { Component } from 'react';
+import './login.css';
+import loginLogo from './id-card.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class LogIn extends Component {
   render() {
     return(
       // <div><h1>Once logged in , this could change to Myprofile :\</h1></div>
-
-      <div>
-        <div className="loginLogo">
-          <p>Logo</p>
-        </div>
+      <div className="loginContainer">
         <div className="mainLogin">
+            <div className="title">
+                <img src={loginLogo} alt="loginLogo"/>
+            </div>
             <form className="loginForm">
               <input className="loginFormInput" type="text" placeholder="Username"></input>
               <br/>
               <input className="loginFormInput" type="text" placeholder="Password"></input>
               <br/>
-              <input className="loginFormInput" type="submit" value="Get Started"></input>
+              <input className="btn" type="submit" value="Login"></input>
             </form>
-        </div>
+            <div className="loginOptionBreak">
+                <p><span>Or Sign In With</span></p>
+            </div>
 
-        <div className="loginOptionBreak">
-          <p>or sign in with</p>        
-        </div>
+            <div className="thirdPartyLogins">
+                {/* <p>Third party portal logins go here</p> */}
+                <a href="#" className="socialIcon">
+                    <FontAwesomeIcon icon={['fab', 'facebook-f']}/>
+                </a>
+                <a href="#" className="socialIcon">
+                    <FontAwesomeIcon icon={['fab', 'google']}/>
+                </a>
+                <a href="#" className="socialIcon">
+                    <FontAwesomeIcon icon={['fab', 'twitter']}/>
+                </a>
+            </div>
 
-        <div className="thirdPartyLogins">
-          {/* <p>Third party portal logins go here</p> */}
-          <a className="thirdPartyLoginButton" href="/login">FaceBook Login Button</a>
-          <a className="thirdPartyLoginButton" href="/login">Google Login Button</a>
-          <a className="thirdPartyLoginButton" href="/login">Twitter Login Button</a>
-        </div>
-
-        <div className="bottomOptionButtons">
-          <a href="/login">Create Account</a>
-          <a href="/ForgotPassword">Forgot Password</a>
+            <div className="bottomOptionButtons">
+                <a href="/login">Create Account</a>
+                <a href="/ForgotPassword">Forgot Password</a>
+            </div>
         </div>
       </div>
     )
