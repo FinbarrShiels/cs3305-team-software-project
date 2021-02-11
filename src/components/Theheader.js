816225a775c9ab6923a42436bd76360781b3bfd7
Luke Tynan <luketynan2000@gmail.com>
2021-02-11 15:12:56 +0000

    small refactor

--- src/components/Theheader.js
+++ src/components/Theheader.js
@@ -7,27 +7,27 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 function Theheader() {
     return(
         <header>
-                <nav>
-                    <div class="menuIcon"><span>
-                        <FontAwesomeIcon icon={['fas','bars']}/>
-                        </span>
-                    </div>
-                    <div onClick={() => {window.location.href="/"}} class="logo"> ShowOfHands </div>
-                    <div class="menuItems">
-                        <li><a href="/login"> Log In  </a></li>
-                        <li><a href="/signUp"> Sign Up </a></li>
-                        <li><a href="/AboutUs"> About Us  </a></li>
-                    </div>
-                    <div class="searchIcon"><span>
-                        <FontAwesomeIcon icon={['fas', 'search']}/>
-                        </span>
-                    </div>
-                    <div class="cancelIcon"><span>
-                        <FontAwesomeIcon icon={['fas', 'times']}/>
-                        </span>
-                    </div>
-                    <div class="search"><SearchBar/></div>
-                </nav>
+            <nav>
+                <div class="menuIcon"><span>
+                    <FontAwesomeIcon icon={['fas','bars']}/>
+                    </span>
+                </div>
+                <div onClick={() => {window.location.href="/"}} class="logo"> ShowOfHands </div>
+                <div class="menuItems">
+                    <li><a href="/login"> Log In  </a></li>
+                    <li><a href="/signUp"> Sign Up </a></li>
+                    <li><a href="/AboutUs"> About Us  </a></li>
+                </div>
+                <div class="searchIcon"><span>
+                    <FontAwesomeIcon icon={['fas', 'search']}/>
+                    </span>
+                </div>
+                <div class="cancelIcon"><span>
+                    <FontAwesomeIcon icon={['fas', 'times']}/>
+                    </span>
+                </div>
+                <div class="search"><SearchBar/></div>
+            </nav>
         </header>
     );
 }
