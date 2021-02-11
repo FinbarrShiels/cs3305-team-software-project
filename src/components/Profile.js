d6c8a678d3e5a44a5dbd2ffcf67626cc1839a165
Luke Tynan <luketynan2000@gmail.com>
2021-02-11 01:41:07 +0000

    Adde basic structure and content of the profile page with working tab changes

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,17 +1,57 @@
-import React, { Component } from 'react';
+import { render } from '@testing-library/react';
+import { useState } from 'react';
 
-class Profile extends Component {        
-  render() {
+function getPage(page) {
+    if (page === 1) {
+        return (
+            <div className="bioTab">
+                <h2>BIO</h2>
+                <button> Edit Bio </button>
+            </div>
+        )
+    }
+    else if (page === 2) {
+        return (
+            <div className="videosTab">
+                <h2>Videos</h2>
+            </div>
+        )
+    }
+    else if (page === 3) {
+        return (
+            <div className="resultsTab">
+                <h2>Results</h2>
+            </div>
+        )
+    }
+}
+
+function Profile() { 
+    const [ page, setPage ] = useState(() => {return 1});
+
     return(
-      <div>
+      <div className="profileCard">
         <div className="left">
-        
+            <div className="profileAvatar"></div>
+            <p className="profileName"> Name </p>
+            <p className="profileOccupation"> Occupation </p>
+            <p className="profileLocation"> Location </p>
+            <div className="profileConnectedAccounts">
+                <div> Twitter </div>
+                <div> GitHub </div>
+                <div> LinkedIn </div>
+            </div>
+            <button className="profileSettings"> Profile Settings </button>
         </div>
         <div className="right">
-
+            <div className="tabOptions">
+                <button onClick={() => {setPage(1)}}> Biography </button>
+                <button onClick={() => {setPage(2)}}> Videos </button>
+                <button onClick={() => {setPage(3)}}> Results </button>
+            </div>
+            {getPage(page)}
         </div>
       </div>
     )
-  }
 }
 export default Profile;
