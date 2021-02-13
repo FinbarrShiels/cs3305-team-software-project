4bb296ecca45b0ea0a9e29f09a2d75d82ca4b070
Luke Tynan <luketynan2000@gmail.com>
2021-02-13 18:10:31 +0000

    Added TeamMemberCard component and base JSX to AboutUs page

--- src/components/pages/AboutUs/AboutUs.js
+++ src/components/pages/AboutUs/AboutUs.js
@@ -1,9 +1,52 @@
 import React, { Component } from 'react';
+import TeamMemberCard from './TeamMemberCard';
 
 class AboutUs extends Component {
     render() {
       return(
-        <h1>About Us section in progress</h1>
+        <div>
+          <div className="memberCards">
+            <TeamMemberCard 
+              avatar="none" 
+              name="Luke" 
+              role="Team Lead / Front End (React)" 
+              statement="some statement"
+            />
+            <TeamMemberCard 
+              avatar="none" name="Finbarr" 
+              role="Front End (CSS)" 
+              statement="some statement"
+            />
+            <TeamMemberCard
+              avatar="none" name="Jack" 
+              role="Front End (Design)" 
+              statement="some statement"
+            />
+            <TeamMemberCard 
+              avatar="none" 
+              name="Diarmuid" 
+              role="Backend" 
+              statement="some statement"
+            />
+            <TeamMemberCard 
+              avatar="none" 
+              name="Con" 
+              role="Backend" 
+              statement="some statement"
+            />
+          </div>
+          <div className="description">
+            <h2>Description</h2>
+            <p>Text</p>
+          </div>
+          <div className="unique">
+            <h2>Unique</h2>
+            <p>Text</p>
+          </div>
+          <div className="projectLinks">
+            GitHub Link
+          </div>
+        </div>
       )
     }
   }
