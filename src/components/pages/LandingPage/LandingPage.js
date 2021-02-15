77da28ae1adebb17ff23f4c8bda6502dcd9ed593
Luke Tynan <luketynan2000@gmail.com>
2021-02-15 13:53:34 +0000

    Added renamed version of LandingPage.js

---
+++ src/components/pages/LandingPage/LandingPage.js
@@ -0,0 +1,21 @@
+import handsUp from '../../Images/handsUp.png';
+import './landing.css'
+
+function LandingPage() {
+  return(
+    <main className="whoops">
+      <img src = {handsUp}
+        alt="Avatar"
+        className="Avatar-img"
+      />
+      <div className="banner-text">
+        <h1>Create and manage your own election.</h1>
+        <hr/>
+        <p>
+          Info on our product .
+        </p>
+      </div>
+    </main>
+  )
+}
+export default LandingPage;
