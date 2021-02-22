525cd72490b19f75d446245110a8456961277e9e
Finbarr Shiels <116765721@umail.ucc.ie>
2021-02-22 11:31:20 +0000

    Refactor and add new menu links

--- src/components/NavBar-Header/Theheader.js
+++ src/components/NavBar-Header/Theheader.js
@@ -13,9 +13,11 @@ function Theheader() {
                 </div>
                 <div onClick={() => {window.location.href="/"}} class="logo"> ShowOfHands </div>
                 <div class="menuItems">
-                    <li><a href="/login"> Log In  </a></li>
-                    <li><a href="/signUp"> Sign Up </a></li>
+                    <li><a href="/">Home</a> </li>
                     <li><a href="/AboutUs"> About Us  </a></li>
+                    <li><a href="/ContactUs">Contact Us</a></li>
+                    <li><a className="loginButton" href="/login">Log In</a></li>
+                    <li><a className="signUpButton" href="/signUp"> Sign Up </a></li>
                 </div>
                 <div class="searchIcon"><span>
                     <FontAwesomeIcon icon={['fas', 'search']}/>
