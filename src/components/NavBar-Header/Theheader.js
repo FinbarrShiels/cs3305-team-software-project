04e6cc1c2125801230c4411d53929691036e54d4
Finbarr Shiels <116765721@umail.ucc.ie>
2021-03-02 18:27:12 +0000

    Small header restyling

--- src/components/NavBar-Header/Theheader.js
+++ src/components/NavBar-Header/Theheader.js
@@ -13,12 +13,14 @@ function Theheader() {
                     </span>
                 </div>
                 <div onClick={() => {window.location.href="/"}} className="logo"> ShowOfHands </div>
+                <div className="search"><SearchBar/></div>
                 <div className="menuItems">
-                    <li><Link to="/login"> Login </Link></li>
-                    <li><Link to="/signup"> Sign Up </Link></li>
+                    <li><Link to="/">Home</Link> </li>
                     <li><Link to="/profile"> Profile</Link></li>
                     <li><Link to="/voting"> Voting </Link></li>
                     <li><Link to="/aboutUs"> About Us </Link></li>
+                    <li><Link className="loginButton" to="/login">Log In</Link></li>
+                    <li><Link className="signUpButton" to="/signUp"> Sign Up </Link></li>
                 </div>
                 <div className="searchIcon"><span>
                     <FontAwesomeIcon icon={['fas', 'search']}/>
@@ -28,7 +30,6 @@ function Theheader() {
                     <FontAwesomeIcon icon={['fas', 'times']}/>
                     </span>
                 </div>
-                <div className="search"><SearchBar/></div>
             </nav>
         </header>
     );
