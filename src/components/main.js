687245732a9cc91bb0844d599613ba5c556e384b
Luke Tynan <luketynan2000@gmail.com>
2021-03-08 23:40:24 +0000

    Added vote information based on pollId in url to vote page

--- src/components/main.js
+++ src/components/main.js
@@ -20,8 +20,8 @@ import Vote from './pages/Voting/Vote.js';
 const Main = () => (
   <Switch>
     <Route exact path="/" component={LandingPage} />
-    <Route path="/signUp" component={signUp} />
-    <Route path="/login" component={LogIn} />
+    <Route path="/SignUp" component={signUp} />
+    <Route path="/Login" component={LogIn} />
     <Route path="/AboutUs" component={AboutUs} />
     <Route path="/ForgotPassword" component={ForgotPassword} />
     <Route path="/RequestSent" component={RequestSent} />
@@ -30,11 +30,11 @@ const Main = () => (
     <Route path="/Search" component={Search}/>
     <Route path="/Profile" component={Profile}/>
     <Route path="/ContactUs" component={ContactUs}/>
-    <Route path="/organise" component={Organise}/>
+    <Route path="/Organise" component={Organise}/>
     <Route path="/latchPage" component={LatchPage}/>
     <Route path="/PasswordChanged" component={PasswordChanged}/>
     <Route path="/SignUpComplete" component={SignUpComplete}/>
-    <Route path="/Vote" component={Vote}/>
+    <Route path="/Vote/:pollId" component={Vote}/>
   </Switch>
 )
 
--- src/components/pages/Voting/Vote.js
+++ src/components/pages/Voting/Vote.js
@@ -1,25 +1,78 @@
 import React, { useEffect } from 'react'
 import queryString from 'query-string'
-import { useHistory } from 'react-router';
-import { useState } from 'react/cjs/react.development';
+import { useParams } from 'react-router'
+import { useState } from 'react/cjs/react.development'
+import { useUser } from '../../../context/UserContext'
+import { getPoll } from '../../../firebaseFunctions/polls'
 
 function Vote() {
 
-    const location = useHistory().location;
-    const query = queryString.parse(location.search); // returns the query object
-    const [ currentVote, setCurrentVote ] = useState()
+    const user = useUser()
+    const { pollId } = useParams()
 
-    useEffect(() => {
-        const params = new URLSearchParams(location.search)
-        const poll = params.get('poll')
-        setCurrentVote(poll)
-    }, [])
+    const getCurrentPoll = () => {
+        getPoll(pollId)
+        .then(newPoll => {
+            setCurrentPoll({
+                options: newPoll.options.map(option => {
+                    return { caption: option.data().option_name }
+                 }),
+                 anon: newPoll.poll.data().anonymousVoters,
+                 createdDate: newPoll.poll.data().createdAt,
+                 desc: newPoll.poll.data().description,
+                 open: newPoll.poll.data().open,
+                 organiser: newPoll.poll.data().organiser,
+                 owners: newPoll.poll.data().owners,
+                 title: newPoll.poll.data().poll_name,
+                 type: newPoll.poll.data().type,
+                 winner: newPoll.poll.data().winner
+             })
+            setFindingPoll(false)
+        })
+        .catch(error => {
+            console.log(error)
+            setFindingPoll(false)
+            setGetPollError(true)
+        })
+    }
+    const [ findingPoll, setFindingPoll ] = useState(true)
+    const [ getPollError, setGetPollError ] = useState(false)
+    const [ currentPoll, setCurrentPoll ] = useState(() => getCurrentPoll())
 
-    return (
-        <div className="voteContainer">
-            {console.log(query.poll)}
-            {`Current Vote: ${currentVote}`}
-        </div>
-    )
+    if (findingPoll) {
+        return <h1> Please wait while we find the vote you're looking for... </h1>
+    }
+    else if (getPollError){
+        return <h1> There was an error while finding the poll you're looking for. Please refresh and try again </h1>
+    }
+    else {
+        return <div className="voteContainer">
+                <h3> {currentPoll.title} </h3>
+                <div className="optionList">
+                    {/* {currentPoll.options.map(option => {
+                        return (
+                            <div className="voteOption">
+                                <p className="optionCaption"> {option} </p>
+                            </div>
+                        )
+                    })} */}
+                </div>
+                <div className="pollInfo">
+                    <div className="organiser">
+                        <p className="organiserName"> {currentPoll.organiser} </p>
+                        <p className="organiserEmail"> {currentPoll.organiser} </p>
+                        <p className="organiserUsername"> {currentPoll.organiser} </p>
+                        <div className="organiserSocials">
+                            <p className="organiserTwitter"> {currentPoll.organiser} </p>
+                            <p className="organiserGitHub"> {currentPoll.organiser} </p>
+                            <p className="organiserLinkedIn"> {currentPoll.organiser} </p>
+                        </div>
+                    </div>
+                    <div className="pollDesc">
+                        <p> {currentPoll.desc} </p>
+                    </div>
+                </div>
+            </div>
+    }
 }
 export default Vote
