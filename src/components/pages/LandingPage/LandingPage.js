7752d3501464172346f15a19c9b82b425c67a3bf
FinbarrShiels <>
2021-03-08 22:39:57 +0000

    Restyle landing page

--- src/components/pages/LandingPage/LandingPage.js
+++ src/components/pages/LandingPage/LandingPage.js
@@ -1,6 +1,7 @@
 import handsUp from '../../Images/vote.jpg';
 import './landing.css'
-
+import computer_vote from "../../Images/computer_vote.png"
+import vote_card from "../../Images/vote_card.png"
 function LandingPage() {
   return(
     <main className="whoops">
@@ -11,13 +12,25 @@ function LandingPage() {
                 <a href="/signup"><button className="heroButton">Start Now</button></a>
             </div>
         </div>
+        <div className="sectionContainer">
       <div className="sectionOne">
-          <div>Image</div>
-          <div>Text</div>
+          <div className="leftImage">
+              <img src={computer_vote} alt="computer_vote"/>
+          </div>
+          <div class="leftText">
+              <h3>Vote for your candidate</h3>
+              <p>Vote for your candidate from any connected device. Secure and simple. </p>
+          </div>
       </div>
         <div className="sectionTwo">
-            <div>Text</div>
-            <div>Image</div>
+            <div className="rightText">
+                <h3>Setup polls or elections</h3>
+                <p>Setup a poll or election for any reason. Work,Union or Class Rep</p>
+            </div>
+            <div className="leftImage">
+                <img src={vote_card} alt="computer_vote"/>
+            </div>
+        </div>
         </div>
     </main>
   )
