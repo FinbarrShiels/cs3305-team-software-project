db501c3256b5d23492cf760f317460e252122502
Luke Tynan <luketynan2000@gmail.com>
2021-03-09 03:02:00 +0000

    Removed react component attribute convention warnings in from console

--- src/components/NavBar-Header/Theheader.js
+++ src/components/NavBar-Header/Theheader.js
@@ -21,15 +21,15 @@ function Theheader() {
                 <svg version="1.1" width="60px" height="60px" viewBox="0 0 131.65 149.04" xmlns="http://www.w3.org/2000/svg">
                     <defs>
                         <linearGradient id="linearGradient882" x1="79.005" x2="42.256" y1="207.72" y2="77.062" gradientUnits="userSpaceOnUse">
-                        <stop stop-color="#0ff" offset="0"/>
-                        <stop stop-color="#00f" offset="1"/>
+                        <stop stopColor="#0ff" offset="0"/>
+                        <stop stopColor="#00f" offset="1"/>
                         </linearGradient>
                     </defs>
-                    <g transform="translate(-39.173 -73.98)" fill="none" stroke="url(#linearGradient882)" stroke-dashoffset="83.896" stroke-linecap="square" stroke-linejoin="round" stroke-miterlimit="3.8">
-                        <rect x="42.256" y="77.062" width="125.49" height="142.88" ry="6.4256" opacity=".997" stroke-width="6.166"/>
-                        <rect x="52.607" y="127.57" width="26.398" height="80.15" rx="1.1909" ry="0" opacity=".997" stroke-width="5.0274"/>
-                        <rect x="91.99" y="97.237" width="26.398" height="110.58" rx="1.1909" ry="0" opacity=".997" stroke-width="5.905"/>
-                        <rect x="131" y="151.01" width="26.398" height="56.42" rx="1.1909" ry="0" opacity=".997" stroke-width="4.218"/>
+                    <g transform="translate(-39.173 -73.98)" fill="none" stroke="url(#linearGradient882)" strokeDashoffset="83.896" strokeLinecap="square" strokeLinejoin="round" strokeMiterlimit="3.8">
+                        <rect x="42.256" y="77.062" width="125.49" height="142.88" ry="6.4256" opacity=".997" strokeWidth="6.166"/>
+                        <rect x="52.607" y="127.57" width="26.398" height="80.15" rx="1.1909" ry="0" opacity=".997" strokeWidth="5.0274"/>
+                        <rect x="91.99" y="97.237" width="26.398" height="110.58" rx="1.1909" ry="0" opacity=".997" strokeWidth="5.905"/>
+                        <rect x="131" y="151.01" width="26.398" height="56.42" rx="1.1909" ry="0" opacity=".997" strokeWidth="4.218"/>
                     </g>
                 </svg>
                 </Link>
@@ -46,7 +46,7 @@ function Theheader() {
                     <li><Link to="/aboutUs"> About Us </Link></li>
                     {user === null && <li><Link className="loginButton" to="/login">Log In</Link></li>}
                     {user === null && <li><Link className="signUpButton" to="/signUp"> Sign Up </Link></li>}
-                    {user !== null && <li> <Link className="logOutButton" onClick={() => userLogOut()}> Log Out </Link></li>}
+                    {user !== null && <li><Link className="logOutButton" to="/login" onClick={() => userLogOut()}> Log Out </Link></li>}
                 </div>
                 <div className="searchIcon"><span>
                     <FontAwesomeIcon icon={['fas', 'search']}/>
