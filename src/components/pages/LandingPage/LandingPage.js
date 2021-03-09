330d743ca72ae94ef0ac8f40a2a95bbe8dc2345c
Luke Tynan <luketynan2000@gmail.com>
2021-03-09 20:36:37 +0000

    Changed landing page buttons when logged in

--- src/components/pages/LandingPage/LandingPage.js
+++ src/components/pages/LandingPage/LandingPage.js
@@ -1,14 +1,22 @@
-import handsUp from '../../Images/vote.jpg';
+import { Link } from 'react-router-dom'
 import './landing.css'
+import { useUser } from '../../../context/UserContext'
 
 function LandingPage() {
+
+  const user = useUser()
+
   return(
     <main className="whoops">
         <div className="heroImage">
             <div className="heroText">
                 <h1><span>Show Of Hands</span></h1>
                 <span className="lowerText">Organise - Vote - Elect</span>
-                <a href="/signup"><button className="heroButton">Start Now</button></a>
+                <div>
+                  { user === null && <Link to="/signup"><button className="heroButton"> Start Now </button></Link> }
+                  { user !== null && <Link to="/organise"><button className="heroButton"> Organise your own vote </button></Link> }
+                  { user !== null && <Link to="/voting"><button className="heroButton"> Find a vote </button></Link> }
+                </div>
             </div>
         </div>
       <div className="sectionOne">
