b1342e61d5bfc00ba6d6f6e8c5b8e60a7cd7d5fa
Luke Tynan <luketynan2000@gmail.com>
2021-03-09 23:50:20 +0000

    Update voting functionality, still lacks API calls and db stores

--- src/components/pages/Voting/Vote.js
+++ src/components/pages/Voting/Vote.js
@@ -44,15 +44,23 @@ function Vote() {
     const [ getPollError, setGetPollError ] = useState(false)
     const [ currentPoll, setCurrentPoll ] = useState(() => getCurrentPoll())
     const [ selectedOption, setSelectedOption ] = useState(null)
+    const [ voteConfirmed, setVoteConfirmed ] = useState({ status: false })
+    const [ voteMsg, setVoteMsg ] = useState("")
 
     const voteForOption = option => {
-        console.log(`Voting for option: ${option.caption} (${option.index})`)
+        console.log(`Voting for: ${option.caption}`)
         setSelectedOption(option)
     }
 
-    useEffect(() => {
-        currentPoll && console.log(currentPoll.options)
-    }, [ currentPoll ])
+    const confirmVote = () => {
+        selectedOption !== null ?
+        setVoteConfirmed({
+            status: true,
+            option: selectedOption
+        })
+        :
+        setVoteMsg("Please click the option you would like to vote for before confirming!")
+    }
 
     if (findingPoll) {
         return <h1> Please wait while we find the vote you're looking for... </h1>
@@ -64,17 +72,23 @@ function Vote() {
         return (
             <div className="voteContainer">
                 <h3> {currentPoll.title} </h3>
-                <div className="optionList">
-                    {currentPoll.options.map((option, index) => {
-                        return (
-                            <div className="voteOption" key={index} onClick={() => voteForOption(option)}>
-                                <p className="optionCaption"> {option.caption} </p>
-                                <p className="optionNumber"> {currentPoll.options.indexOf(option)} </p>
-                            </div>
-                        )
-                    })}
-                </div>
-                <button onClick={() => console.log(`Confirming Vote: ${selectedOption.caption}`)}> Confirm Vote </button>
+                <p className="voteMsg"> {voteMsg} </p>
+                { !voteConfirmed.status
+                ? 
+                    <div className="optionList">
+                        {currentPoll.options.map((option, index) => {
+                            return (
+                                <div className="voteOption" key={index} onClick={() => voteForOption(option, currentPoll.options.indexOf(option))}>
+                                    <p className="optionCaption"> {option.caption} </p>
+                                    <p className="optionNumber"> {currentPoll.options.indexOf(option)} </p>
+                                </div>
+                            )
+                        })}
+                        <button onClick={() => confirmVote()}> Confirm Vote </button>
+                    </div>
+                :
+                    <p className="alreadyVoted"> Your vote has been confirmed! You voted for: {voteConfirmed.option.caption} </p>
+                }
                 <div className="pollInfo">
                     <div className="organiser">
                         <p className="organiserName"> {currentPoll.organiser.fname} {currentPoll.organiser.sname} </p>
