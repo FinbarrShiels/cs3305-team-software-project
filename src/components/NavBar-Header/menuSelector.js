f66def5acffde6cb675e84a653e7d83aff7a59e7
Luke Tynan <luketynan2000@gmail.com>
2021-03-09 11:55:15 +0000

    Removed buggy menuSelector.js

--- src/components/NavBar-Header/menuSelector.js
+++ src/components/NavBar-Header/menuSelector.js
@@ -1,28 +1,28 @@
-window.addEventListener ('DOMContentLoaded', () => {
-    const mBtn = document.querySelector(".menuIcon span");
-    const searchBtn = document.querySelector(".searchIcon");
-    const canBtn = document.querySelector(".cancelIcon");
-    const items = document.querySelector(".menuItems");
-    const search = document.querySelector(".search");
-    mBtn.onclick = () => {
-        items.classList.add("active");
-        mBtn.classList.add("hide");
-        searchBtn.classList.add("hide");
-        canBtn.classList.add("show");
-    }
-    canBtn.onclick = () => {
-        items.classList.remove("active");
-        mBtn.classList.remove("hide");
-        searchBtn.classList.remove("hide");
-        canBtn.classList.remove("show")
-        search.classList.remove("active");
-    }
-    searchBtn.onclick = () => {
-        search.classList.add("active");
-        searchBtn.classList.add("hide");
-        canBtn.classList.add("show");
-    }
-});
+// window.addEventListener ('DOMContentLoaded', () => {
+//     const mBtn = document.querySelector(".menuIcon span");
+//     const searchBtn = document.querySelector(".searchIcon");
+//     const canBtn = document.querySelector(".cancelIcon");
+//     const items = document.querySelector(".menuItems");
+//     const search = document.querySelector(".search");
+//     mBtn.onclick = () => {
+//         items.classList.add("active");
+//         mBtn.classList.add("hide");
+//         searchBtn.classList.add("hide");
+//         canBtn.classList.add("show");
+//     }
+//     canBtn.onclick = () => {
+//         items.classList.remove("active");
+//         mBtn.classList.remove("hide");
+//         searchBtn.classList.remove("hide");
+//         canBtn.classList.remove("show")
+//         search.classList.remove("active");
+//     }
+//     searchBtn.onclick = () => {
+//         search.classList.add("active");
+//         searchBtn.classList.add("hide");
+//         canBtn.classList.add("show");
+//     }
+// });
 
 
 
