a75db4a0b527b295c0aeef6d91f36e306368f535
FinbarrShiels <>
2021-03-16 23:35:08 +0000

    Fix menu links to stay active in NavBar

--- src/components/NavBar-Header/Theheader.js
+++ src/components/NavBar-Header/Theheader.js
@@ -1,10 +1,9 @@
 import './Theheader.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import "./menuSelector.js"
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import { useUser, useUserLogOut } from "../../context/UserContext"
 import "../pages/Search/SearchBar.css"
-
 function Theheader() {
 
     const user = useUser();
@@ -13,7 +12,7 @@ function Theheader() {
     return(
         <header>
             <nav>
-                <Link to="/">  
+                <NavLink to="/">
                 <svg className="logo" width="80" height="80" data-name="Layer 1" version="1.1" viewBox="0 0 63.546 95.922" xmlns="http://www.w3.org/2000/svg">
                     <defs>
                     <linearGradient id="linear-gradient" x1="149.07" x2="149.07" y1="71.303" y2="167.15" gradientTransform="translate(-117.3 -84.928)" gradientUnits="userSpaceOnUse">
@@ -23,7 +22,7 @@ function Theheader() {
                     </defs>
                     <path className="cls-1" d="m27.898 3.8748a3.886 3.886 0 0 1 3.8747-3.8748 3.886 3.886 0 0 1 3.8747 3.8748v60.331a3.886 3.886 0 0 1-3.8747 3.8745 3.886 3.886 0 0 1-3.8747-3.8745zm-13.949 6.6663a3.8747 3.8747 0 0 1 7.7494 0v53.512a10.174 10.174 0 0 0 9.9179 10.226c0.0522 7e-4 0.1045 1e-3 0.1565 1e-3a10.086 10.086 0 0 0 10.074-10.074v-53.665a3.8747 3.8747 0 0 1 7.7494 0v53.468a17.951 17.951 0 0 1-17.72 18.02h-0.104a17.843 17.843 0 0 1-17.824-17.824zm49.597 53.665a31.773 31.773 0 0 1-63.546 0v-20.97a3.8748 3.8748 0 1 1 7.7495 0v20.97a24.05 24.05 0 0 0 24.023 24.023h0.068a24.174 24.174 0 0 0 23.955-24.256v-39.328a3.875 3.875 0 1 1 7.75 0z" fill="url(#linear-gradient)"/>
                 </svg>
-                </Link>
+                </NavLink>
                 {/* <div className="search">
                     <div className="searchBar">
                     <input type="search" placeholder="Search by Name..."/>
@@ -35,14 +34,14 @@ function Theheader() {
                     </span>
                 </div>
                 <div className="menuItems">
-                    <li><Link to="/">Home</Link></li>
-                    {user !== null && <li><Link to="/profile"> Profile </Link></li>}
-                    <li><Link to="/search"> Voting </Link></li>
-                    <li><Link to="/aboutUs"> About Us </Link></li>
-                    {user === null && <li><Link className="loginButton" to="/login">Log In</Link></li>}
-                    {user === null && <li><Link className="signUpButton" to="/signUp"> Sign Up </Link></li>}
-                    {user !== null && <li><Link className="logOutButton" to="/login" onClick={() => userLogOut()}> Log Out </Link></li>}
-                    {user !== null && user.fname && user.sname && <p><Link to="/profile"> {`${user.fname} ${user.sname}`} </Link></p>}
+                    <li><NavLink exact={true} to="/">Home</NavLink></li>
+                    {user !== null && <li><NavLink to="/profile"> Profile </NavLink></li>}
+                    <li><NavLink to="/search"> Voting </NavLink></li>
+                    <li><NavLink to="/aboutUs"> About Us </NavLink></li>
+                    {user === null && <li><NavLink className="loginButton" to="/login">Log In</NavLink></li>}
+                    {user === null && <li><NavLink className="signUpButton" to="/signUp"> Sign Up </NavLink></li>}
+                    {user !== null && <li><NavLink className="logOutButton" to="/logip" onClick={() => userLogOut()}> Log Out </NavLink></li>}
+                    {user !== null && user.fname && user.sname && <p><NavLink to="/profile"> {`${user.fname} ${user.sname}`} </NavLink></p>}
                 </div>
                 {/* <div className="searchIcon"><span>
                     <FontAwesomeIcon icon={['fas', 'search']}/>
